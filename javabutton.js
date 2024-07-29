a2aa78d56734c35d12e744302d43a8c0de5b8eb4
Jasmine <jasminepbutler@gmail.com>
2024-07-29 08:05:30 -0500

    java button connect.html

--- javabutton.js
+++ javabutton.js
@@ -1,7 +1,15 @@
-function JavaBa () {
-    alert("Welcome to my website..")
+function JavaBa() {
+    alert("Welcome to my website..");
 }
 
 JavaB.onclick = JavaBa;
 
 
+function myFunclick1() {
+    var x = document.getElementById("javaJaz1");
+    if (x.innerHTML === "Jaz's capstone") {
+      x.innerHTML = "..is awesome";
+    } else {
+      x.innerHTML = "Jaz's capstone";
+    }
+  }
